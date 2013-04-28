c7a25c10e09204500e6a5e29699fccde3fb22e6a
chesterbr <cd@pobox.com>
2013-04-28 11:15:29 -0400

    Innocent DEX implementation
    (coupled fetch+execute; implicit decode)

--- lib/ruby_atari/cpu.rb
+++ lib/ruby_atari/cpu.rb
@@ -1,10 +1,17 @@
 class Cpu
-  attr_accessor :memory, :pc
+  attr_accessor :memory, :pc, :x
 
   RESET_VECTOR = 0xFFFC
 
   def reset
     self.pc = memory[RESET_VECTOR] + 0x100 * memory[RESET_VECTOR + 1]
+  end
+
+  def next
+    #... decoded as DEX (baby steps!)
+    self.x -= 1
+    self.pc += 1
+    2
   end
 
 end
--- spec/lib/cpu_spec.rb
+++ spec/lib/cpu_spec.rb
@@ -8,7 +8,30 @@ describe Cpu do
 
     it 'should boot at the address on the RESET vector ($FFFC)' do
       cpu.reset
+
       cpu.pc.should == 0x1234
+    end
+  end
+
+  describe '#next' do
+    before do
+      cpu.memory = [0xCA] # DEX
+      cpu.pc = 0x0000
+      cpu.x  = 0x07
+    end
+
+    it 'should execute an instruction' do
+      cpu.next
+
+      cpu.x.should == 0x06
+    end
+
+    it 'should advance PC to the following instruction' do
+      expect { cpu.next }.to change { cpu.pc }.by(1)
+    end
+
+    it 'should return the "time" (# of CPU cyles) taken by an instruction' do
+      cpu.next.should == 2
     end
   end
 end
