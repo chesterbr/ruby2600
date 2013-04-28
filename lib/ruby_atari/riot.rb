078f9ee44b884ace423bad8f938ced083630bc8b
chesterbr <cd@pobox.com>
2013-04-28 10:22:21 -0400

    Making RIOT array-ish as well

--- lib/ruby_atari/riot.rb
+++ lib/ruby_atari/riot.rb
@@ -3,11 +3,11 @@ class Riot
     @ram = Array.new(128)
   end
 
-  def read(position)
+  def [](position)
     @ram[position - 0x80]
   end
 
-  def write(position, value)
+  def []=(position, value)
     @ram[position - 0x80] = value
   end
 end
--- spec/lib/riot_spec.rb
+++ spec/lib/riot_spec.rb
@@ -4,8 +4,8 @@ describe Riot do
   it 'should store values on the RAM range' do
     (0x80..0xFF).each do |position|
       value = Random.rand(256)
-      subject.write(position , value)
-      subject.read(position).should be(value), "Failed for value $#{value.to_s(16)} at $#{position.to_s(16)}"
+      subject[position] = value
+      subject[position].should be(value), "Failed for value $#{value.to_s(16)} at $#{position.to_s(16)}"
     end
   end
 end
