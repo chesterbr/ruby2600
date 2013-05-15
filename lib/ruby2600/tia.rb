528e93bb8d6d2d27cde7010ec72e79bf54988c14
chesterbr <cd@pobox.com>
2013-05-14 23:20:32 -0400

    Playfield generation (basic PF0-PF3, no mirroring/score mode)

--- lib/ruby2600/tia.rb
+++ lib/ruby2600/tia.rb
@@ -1,10 +1,10 @@
 module Ruby2600
   class TIA
     include Constants
-    attr_accessor :pf0, :pf1, :pf2, :colubk, :colupf
 
     def initialize
-      @reg = Array(32)
+      @reg = Array.new(32)
+      @scanline = Array.new(160)
     end
 
     def [](position)
@@ -16,7 +16,41 @@ module Ruby2600
     end
 
     def scanline
-      Array.new(160, @reg[PF0] == 0 ? @reg[COLUBK] : @reg[COLUPF])
+      pf_reset
+      (0..159).each do |pixel|
+        @pixel = pixel
+        @scanline[pixel] = pf_bit.nonzero? ? @reg[COLUPF] : @reg[COLUBK]
+        pf_fetch
+      end
+      @scanline
+    end
+
+    private
+
+    # Playfield
+
+    def pf_reset
+      @pf_reg = PF0
+      @pf_bit = 4
+      @pf_direction = 1
+    end
+
+    def pf_bit
+      @reg[@pf_reg][@pf_bit]
+    end
+
+    def pf_fetch
+      if @pixel % 80 % 4 == 3
+        @pf_bit += @pf_direction
+        pf_flip_direction_and_register if @pf_bit == 8 || @pf_bit == -1
+        pf_reset if @pf_reg > PF2
+      end
+    end
+
+    def pf_flip_direction_and_register
+      @pf_direction = -@pf_direction
+      @pf_bit += @pf_direction
+      @pf_reg += 1
     end
   end
 end
