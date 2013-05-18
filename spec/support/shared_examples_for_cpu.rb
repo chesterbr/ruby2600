c3951b576fe850da3548a899f28c0efe6533c9c7
chesterbr <cd@pobox.com>
2013-05-18 01:04:52 -0400

    Fixing shared example scope

--- spec/support/shared_examples_for_cpu.rb
+++ spec/support/shared_examples_for_cpu.rb
@@ -170,40 +170,39 @@ shared_examples_for "a branch instruction" do
       it_should 'preserve flags'
     end
   end
-
-  shared_examples_for 'compare and set flags' do |register, value|
-    context 'register < value' do
-      before { cpu.send("#{register}=", value - 1) }
-
-      it_should 'set N flag'
-
-      it_should 'reset C flag'
-
-      it_should 'reset Z flag'
-    end
-
-    context 'register = value' do
-      before { cpu.send("#{register}=", value) }
-
-      it_should 'reset N flag'
-
-      it_should 'set C flag'
-
-      it_should 'set Z flag'
-    end
-
-    context 'register > value' do
-      before { cpu.send("#{register}=", value + 1) }
-
-      it_should 'reset N flag'
-
-      it_should 'set C flag'
-
-      it_should 'reset Z flag'
-    end
-  end
 end
 
+shared_examples_for 'compare and set flags' do |register, value|
+  context 'register < value' do
+    before { cpu.send("#{register}=", value - 1) }
+
+    it_should 'set N flag'
+
+    it_should 'reset C flag'
+
+    it_should 'reset Z flag'
+  end
+
+  context 'register = value' do
+    before { cpu.send("#{register}=", value) }
+
+    it_should 'reset N flag'
+
+    it_should 'set C flag'
+
+    it_should 'set Z flag'
+  end
+
+  context 'register > value' do
+    before { cpu.send("#{register}=", value + 1) }
+
+    it_should 'reset N flag'
+
+    it_should 'set C flag'
+
+    it_should 'reset Z flag'
+  end
+end
 
 shared_examples_for 'work on any memory position' do
   it 'works on lower 1KB' do
