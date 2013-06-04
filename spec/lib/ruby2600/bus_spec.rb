eb77d31d7c75406a63d38f4238efc153c18cfeb4
chesterbr <cd@pobox.com>
2013-06-03 21:03:58 -0400

    A decent spec for chip selection and address mirroring
    using info from http://atariage.com/forums/topic/192418-mirrored-memory/

--- spec/lib/ruby2600/bus_spec.rb
+++ spec/lib/ruby2600/bus_spec.rb
@@ -9,12 +9,20 @@ describe Ruby2600::Bus do
 
   subject(:bus) { Ruby2600::Bus.new(cpu, tia, cart, riot) }
 
+  # 6507 address lines (bits) A7 & A12 select which chip (TIA/RIOT/cart ROM)
+  # will be read/written. See: http://atariage.com/forums/topic/192418-mirrored-memory
+
+  ALL_ADDRESSES  = (0x0000..0xFFFF).to_a
+  CART_ADDRESSES = ALL_ADDRESSES.select { |a| a[12] == 1 }
+  RIOT_ADDRESSES = ALL_ADDRESSES.select { |a| a[12] == 0 && a[7] == 0 }
+  TIA_ADDRESSES  = ALL_ADDRESSES.select { |a| a[12] == 0 && a[7] == 1 }
+
   context 'initialization' do
     it 'should wire itself as a memory proxy for CPU' do
       cpu.should_receive(:memory=).with(bus)
     end
 
-    it 'should wire TIA to CPU' do
+    it 'should wire TIA to CPU (so it can drive the CPU timing)' do
       tia.should_receive(:cpu=).with(cpu)
 
       bus
@@ -27,26 +35,64 @@ describe Ruby2600::Bus do
     end
   end
 
+  # Since we only want to check if the values are read
+  # from the right chip/address, we'll use arrays as stubs
+
   describe '#read' do
-    it_should 'read address range from tia',  0x0000..0x000D
-    it_should 'read address range from riot', 0x0080..0x00FF
+    let(:cart) { Array.new(4096) { rand(256) } }
+    let(:tia)  { Array.new(64)   { rand(256) } }
+    let(:riot) { Array.new(128)  { rand(256) } }
 
-    it "should translate reads on address range $F000-FFFF to lower 4K range on cart" do
-      0.upto(4095).each do |address|
-        value = Random.rand(256)
-        cart.stub(:[]).with(address).and_return(value)
+    before { tia.stub(:cpu=) }
 
-        bus[0xF000 + address].should == value
+    it 'reads from TIA' do
+      TIA_ADDRESSES.each do |a|
+        bus[a].should == tia[a &  0b0000000000111111]
       end
     end
 
+    it 'reads from RIOT' do
+      RIOT_ADDRESSES.each do |a|
+        bus[a].should == riot[a & 0b0000000001111111]
+      end
+    end
+
+    it 'reads from CART (ROM)' do
+      CART_ADDRESSES.each do |a|
+        bus[a].should == cart[a & 0b0001111111111111]
+      end
+    end
   end
+
+  # Here we want to ensure that writes to the bus will trigger
+  # array-sytle writes on the chips, so shortcuts won't do
 
   describe '#write' do
-    it_should 'write address range to tia',  0x0000..0x002C
-    it_should 'write address range to riot', 0x0080..0x00FF
+    it 'writes to TIA' do
+      TIA_ADDRESSES.each do |a|
+        value = rand(256)
+        tia.should_receive(:[]).with(a & 0b0000000000111111, value)
+
+        bus[a] = value
+      end
+    end
+
+    it 'writes to RIOT' do
+      RIOT_ADDRESSES.each do |a|
+        value = rand(256)
+        riot.should_receive(:[]).with(a & 0b0000000001111111, value)
+
+        bus[a] = value
+      end
+    end
+
+    it 'does NOT write to cart (ROM == Read-Only-Memory)' do
+      CART_ADDRESSES.each do |a|
+        bus[a] = rand(256) # no mock should receive any message
+      end
+    end
   end
 
-  # TODO mirroring (TIA, at least, is mirrored in several pages)
+  pending '2K carts / SARA / RIOT details from http://atariage.com/forums/topic/192418-mirrored-memory/'
 
 end
