8001930d6025c9c61f66be40b2c58f5a383422a6
chesterbr <cd@pobox.com>
2013-07-23 22:48:29 -0400

    Testing all NUSIZ combinations

--- spec/lib/ruby2600/player_spec.rb
+++ spec/lib/ruby2600/player_spec.rb
@@ -37,47 +37,166 @@ describe Ruby2600::Player do
       300.times { player.pixel.should be_nil }
     end
 
-    context 'player strobing' do
+    context 'drawing (strobe, NUSIZ0)' do
       COLOR = 2 * (rand(127) + 1)
-      PATTERN_PIXELS = [COLOR, COLOR, nil, nil, COLOR, nil, COLOR, nil]
-      PATTERN_BITS   = 0b11001010
+      PIXELS = [COLOR, COLOR, nil, nil, COLOR, nil, COLOR, nil]
+      PIXELS_2X = PIXELS.map    { |p| [p, p] }.flatten
+      PIXELS_4X = PIXELS_2X.map { |p| [p, p] }.flatten
 
       before do
-        tia[GRP0]   = PATTERN_BITS
-        tia[NUSIZ0] = 0                    # no repetition
-        tia[COLUP0] = COLOR
-        rand(160).times { player.pixel }   # arbitrary screen position
+        # Cleanup and pick a random position
+        tia[GRP0] = 0
+        rand(160).times { player.pixel }
+
+        # Preemptive strobe (to ensure we don't have retriggering leftovers)
         player.strobe
+        80.times { player.pixel }
+
+        # Setup
+        tia[GRP0] = 0b11001010
+        tia[COLUP0] = COLOR
       end
 
-      it 'should output the player after a full scanline (160pixels) + 5-bit delay' do
-        # Player is drawn on next scanline (160 pixels), delayed by 5 pixels
-        165.times { player.pixel }
-        pixels(player, 1, 8).should == PATTERN_PIXELS
+      context 'one copy' do
+        before do
+          tia[NUSIZ0] = 0
+          player.strobe
+        end
+
+        it 'should not draw anything on current scanline' do
+          5.times { player.pixel }
+          pixels(player, 1, 160).should == Array.new(160)
+        end
+
+        it 'should draw after a full scanline (160pixels) + 5-bit delay' do
+          165.times { player.pixel }
+          pixels(player, 1, 8).should == PIXELS
+        end
+
+        it 'should draw again on subsequent scanlines' do
+          325.times { player.pixel }
+          5.times { pixels(player, 1, 160).should == PIXELS + Array.new(152) }
+        end
       end
 
-      xit 'should draw player again on second-after-current scanline' do
-        325.times { player.pixel }
-        pixels(player, 1, 8).should == PATTERN_PIXELS
-      end
-
-      it 'should draw player on third-after-current scanline' do
-        485.times { player.pixel }
-        pixels(player, 1, 8).should == PATTERN_PIXELS
-      end
-
-      context 'two copies' do
+      context 'two copies, close' do
         before do
           tia[NUSIZ0] = 1
           player.strobe
         end
 
-        it 'should draw the second copy of the player on *current* scanline (after 5 bit delay)' do
+        it 'should only draw second copy on current scanline (after 5 bit delay)' do
           5.times { player.pixel }
-          pixels(player, 17, 24).should == PATTERN_PIXELS
+          pixels(player, 1, 24).should == Array.new(16) + PIXELS
+        end
+
+        it 'should draw both copies on subsequent scanlines' do
+          165.times { player.pixel }
+          pixels(player, 1, 24).should == PIXELS + Array.new(8) + PIXELS
         end
       end
-    end
 
+      context 'two copies, medium' do
+        before do
+          tia[NUSIZ0] = 2
+          player.strobe
+        end
+
+        it 'should only draw second copy on current scanline (after 5 bit delay)' do
+          5.times { player.pixel }
+          pixels(player, 1, 40).should == Array.new(32) + PIXELS
+        end
+
+        it 'should draw both copies on subsequent scanlines' do
+          165.times { player.pixel }
+          pixels(player, 1, 40).should == PIXELS + Array.new(24) + PIXELS
+        end
+      end
+
+      context 'three copies, close' do
+        before do
+          tia[NUSIZ0] = 3
+          player.strobe
+        end
+
+        it 'should only draw second and third copy on current scanline (after 5 bit delay)' do
+          5.times { player.pixel }
+          pixels(player, 1, 40).should == Array.new(16) + PIXELS + Array.new(8) + PIXELS
+        end
+
+        it 'should draw three copies on subsequent scanlines' do
+          165.times { player.pixel }
+          pixels(player, 1, 40).should == PIXELS + Array.new(8) + PIXELS + Array.new(8) + PIXELS
+        end
+      end
+
+      context 'two copies, wide' do
+        before do
+          tia[NUSIZ0] = 4
+          player.strobe
+        end
+
+        it 'should only draw second copy on current scanline (after 5 bit delay)' do
+          5.times { player.pixel }
+          pixels(player, 1, 72).should == Array.new(64) + PIXELS
+        end
+
+        it 'should draw both copies on subsequent scanlines' do
+          165.times { player.pixel }
+          pixels(player, 1, 72).should == PIXELS + Array.new(56) + PIXELS
+        end
+      end
+
+      context 'one copy, double size' do
+        before do
+          tia[NUSIZ0] = 5
+          player.strobe
+        end
+
+        it 'should not draw anything on current scanline' do
+          5.times { player.pixel }
+          pixels(player, 1, 160).should == Array.new(160)
+        end
+
+        it 'should draw on subsequent scanlines' do
+          165.times { player.pixel }
+          5.times { pixels(player, 1, 160).should == PIXELS_2X + Array.new(144) }
+        end
+      end
+
+      context 'three copies, medium' do
+        before do
+          tia[NUSIZ0] = 6
+          player.strobe
+        end
+
+        it 'should only draw second and third copy on current scanline (after 5 bit delay)' do
+          5.times { player.pixel }
+          pixels(player, 1, 72).should == Array.new(32) + PIXELS + Array.new(24) + PIXELS
+        end
+
+        it 'should draw three copies on subsequent scanlines' do
+          165.times { player.pixel }
+          pixels(player, 1, 72).should == PIXELS + Array.new(24) + PIXELS + Array.new(24) + PIXELS
+        end
+      end
+
+      context 'one copy, quad size' do
+        before do
+          tia[NUSIZ0] = 7
+          player.strobe
+        end
+
+        it 'should not draw anything on current scanline' do
+          5.times { player.pixel }
+          pixels(player, 1, 160).should == Array.new(160)
+        end
+
+        it 'should draw on subsequent scanlines' do
+          165.times { player.pixel }
+          5.times { pixels(player, 1, 160).should == PIXELS_4X + Array.new(128) }
+        end
+      end      
+    end
   end
 end
