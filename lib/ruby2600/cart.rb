331ea2aaf61f5cca35fbf6351c9547743b2ba8a1
chesterbr <cd@pobox.com>
2013-08-04 01:19:22 -0400

    2K cart support

--- lib/ruby2600/cart.rb
+++ lib/ruby2600/cart.rb
@@ -2,6 +2,7 @@ module Ruby2600
   class Cart
     def initialize(rom_file)
       @bytes = File.open(rom_file, "rb") { |f| f.read }.unpack('C*')
+      @bytes += @bytes if @bytes.count == 2048
     end
 
     def [](address)
--- spec/lib/ruby2600/cart_spec.rb
+++ spec/lib/ruby2600/cart_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Ruby2600::Cart do
-  let(:cart_4K) { Ruby2600::Cart.new(path_for_ROM :hello) }
+  let(:cart_4K) { Ruby2600::Cart.new(path_for_ROM :hello)   }
+  let(:cart_2K) { Ruby2600::Cart.new(path_for_ROM :hello2k) }
 
   it 'should silently ignore writes' do
     old_value = cart_4K[0x0000]
@@ -10,11 +11,25 @@ describe Ruby2600::Cart do
     cart_4K[0x0000].should == old_value
   end
 
-  it 'should map a 4K ROM' do
-    cart_4K[0x0000].should == 0xA9       # F000: LDA #02 (first instruction)
+  it 'should load a 2K ROM' do
+    cart_2K[0x0000].should == 0xA9       # _000: LDA #02 (first instruction)
+    cart_2K[0x0001].should == 0x02
+
+    cart_2K[0x07FE].should == 0x00       # _FFE: word $F800     (BRK vector)
+    cart_2K[0x07FF].should == 0XF8
+  end
+
+  it 'should load a 4K ROM' do
+    cart_4K[0x0000].should == 0xA9       # _000: LDA #02 (first instruction)
     cart_4K[0x0001].should == 0x02
 
-    cart_4K[0x0FFE].should == 0x00       # FFFE: word $F000     (BRK vector)
+    cart_4K[0x0FFE].should == 0x00       # _FFE: word $F000     (BRK vector)
     cart_4K[0x0FFF].should == 0XF0
+  end
+
+  it 'should map a 2K ROM as a doubled 4k' do
+    0x0000.upto(0x07FF) do |addr|
+      cart_2K[addr].should == cart_2K[addr + 2048]
+    end
   end
 end
