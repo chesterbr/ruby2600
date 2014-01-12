5c6c0f6dde5523a2564c923caa2467312d00c5d9
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2014-01-11 20:08:20 -0500

    cleanup

--- spec/functional/vertical_delay_spec.rb
+++ spec/functional/vertical_delay_spec.rb
@@ -75,14 +75,12 @@ describe 'vertical delay' do
     end
 
     it "should use new register (disabled) by default" do
-      puts tia.scanline.to_s
       ball_on_scanline.should_not == 0x22
     end
 
     it "should use old register (enabled) if VDELP0 bit 0 is set" do
       pending "intermitent failures on this test, fix it"
       tia[VDELBL] = rand(256) & 1
-      puts tia.scanline.to_s
       ball_on_scanline.should == 0x22
     end
   end
